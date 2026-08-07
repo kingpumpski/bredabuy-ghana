54bb1d0c0e36ac55bfabfab52193980f9bb2c606
Kingsley Norku <32315099+kingpumpski@users.noreply.github.com>
2026-08-07 14:35:45 +0000

    fix: restore application bootstrap stability

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
+
 import { CartProvider } from "@/context/CartContext";
 import { ThemeProvider } from "@/context/ThemeContext";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import Index from "./pages/Index";
 import Products from "./pages/Products";
 import ProductDetail from "./pages/ProductDetail";
@@ -14,33 +18,85 @@ import Checkout from "./pages/Checkout";
 import OrderSuccess from "./pages/OrderSuccess";
 import NotFound from "./pages/NotFound";
 
+
 const queryClient = new QueryClient();
 
-const App = () => (
-  <HelmetProvider>
+
+const App = () => {
+
+  return (
+
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <CartProvider>
-          <TooltipProvider>
-            <Toaster />
-            <Sonner />
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="/product/:id" element={<ProductDetail />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/order-success" element={<OrderSuccess />} />
-                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </BrowserRouter>
-          </TooltipProvider>
-        </CartProvider>
-      </ThemeProvider>
+
+      <HelmetProvider>
+
+        <ThemeProvider>
+
+          <CartProvider>
+
+            <TooltipProvider>
+
+              <Toaster />
+
+              <Sonner />
+
+
+              <BrowserRouter>
+
+                <Routes>
+
+                  <Route
+                    path="/"
+                    element={<Index />}
+                  />
+
+                  <Route
+                    path="/products"
+                    element={<Products />}
+                  />
+
+                  <Route
+                    path="/product/:id"
+                    element={<ProductDetail />}
+                  />
+
+                  <Route
+                    path="/cart"
+                    element={<Cart />}
+                  />
+
+                  <Route
+                    path="/checkout"
+                    element={<Checkout />}
+                  />
+
+                  <Route
+                    path="/order-success"
+                    element={<OrderSuccess />}
+                  />
+
+                  <Route
+                    path="*"
+                    element={<NotFound />}
+                  />
+
+                </Routes>
+
+              </BrowserRouter>
+
+
+            </TooltipProvider>
+
+          </CartProvider>
+
+        </ThemeProvider>
+
+      </HelmetProvider>
+
     </QueryClientProvider>
-  </HelmetProvider>
-);
+
+  );
+};
+
 
 export default App;
