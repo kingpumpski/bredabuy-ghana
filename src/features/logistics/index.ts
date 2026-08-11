f967472e6d3fa10a5935195fe7aad436615a65b3
Kingsley Norku <32315099+kingpumpski@users.noreply.github.com>
2026-08-11 11:42:02 +0000

    feat: export delivery exception management

--- src/features/logistics/index.ts
+++ src/features/logistics/index.ts
@@ -2,4 +2,5 @@ export * from "./services/shipment.service";
 export * from "./services/fulfilment-shipment.service";
 export * from "./services/carrier.service";
 export * from "./services/shipment-assignment.service";
+export * from "./services/delivery-exception.service";
 export * from "./types/logistics.types";
