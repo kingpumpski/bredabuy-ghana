374eac193cc25524d1001d9c9eb120d8fb79993a
Kingsley Norku <32315099+kingpumpski@users.noreply.github.com>
2026-08-11 11:45:28 +0000

    feat: export return inspection workflow

--- src/features/logistics/index.ts
+++ src/features/logistics/index.ts
@@ -4,4 +4,5 @@ export * from "./services/carrier.service";
 export * from "./services/shipment-assignment.service";
 export * from "./services/delivery-exception.service";
 export * from "./services/return-shipment.service";
+export * from "./services/return-inspection.service";
 export * from "./types/logistics.types";
