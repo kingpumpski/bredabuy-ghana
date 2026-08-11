972d00ea838aeeee975d6b9b13bcaad982d554bd
Kingsley Norku <32315099+kingpumpski@users.noreply.github.com>
2026-08-11 11:38:58 +0000

    feat: export carrier management

--- src/features/logistics/index.ts
+++ src/features/logistics/index.ts
@@ -1,3 +1,4 @@
 export * from "./services/shipment.service";
 export * from "./services/fulfilment-shipment.service";
+export * from "./services/carrier.service";
 export * from "./types/logistics.types";
