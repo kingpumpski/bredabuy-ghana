aa81b890ce189455a178f01c4154ef8a3fca0b3a
Kingsley Norku <32315099+kingpumpski@users.noreply.github.com>
2026-08-11 11:40:17 +0000

    feat: export shipment assignment service

--- src/features/logistics/index.ts
+++ src/features/logistics/index.ts
@@ -1,4 +1,5 @@
 export * from "./services/shipment.service";
 export * from "./services/fulfilment-shipment.service";
 export * from "./services/carrier.service";
+export * from "./services/shipment-assignment.service";
 export * from "./types/logistics.types";
