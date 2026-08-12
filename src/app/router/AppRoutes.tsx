ef7634b2f2133454dfdd74c7494784fc90a373e1
Kingsley Norku <32315099+kingpumpski@users.noreply.github.com>
2026-08-12 11:07:31 +0000

    refactor: remove unused duplicate router wrapper

--- src/app/router/AppRoutes.tsx
+++
@@ -1,9 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-
-import router from "./routes";
-
-export function AppRoutes() {
-  return <RouterProvider router={router} />;
-}
-
-export default AppRoutes;
